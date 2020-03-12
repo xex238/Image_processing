398b34700d28ae84d6666318df59de34730549fd
Дмитрий <32355073+xex238@users.noreply.github.com>
2020-03-12 22:15:43 +0300

    Update lab_03.cpp

--- lab_03/lab_03.cpp
+++ lab_03/lab_03.cpp
@@ -9,6 +9,7 @@
 
 #include <iostream>
 #include <ostream>
+#include <vector>
 
 using namespace cv;
 using namespace std;
@@ -185,7 +186,6 @@ Mat Step_Otsu_binarization(Mat& bw_mat)
 
 	return bw_mat;
 }
-
 Mat Global_binarization(Mat& bw_mat)
 {
 	//Mat result = Step_average_binarization(bw_mat);
@@ -198,9 +198,392 @@ Mat Global_binarization(Mat& bw_mat)
 	return bw_mat_2;
 }
 
+Mat Image_extention_constant(Mat& mat, int x, int y, int constant)
+{
+	Mat result(mat.rows + y - 1, mat.cols + x - 1, mat.type(), Scalar(constant));
+
+	// Заполняем основную сцену нового изображения
+	int i_original = 0;
+	for (int i = round(y / 2); i < result.rows - round(y / 2); i++)
+	{
+		int j_original = 0;
+		for (int j = round(x / 2); j < result.cols - round(x / 2); j++)
+		{
+			result.at<uint8_t>(i, j) = mat.at<uint8_t>(i_original, j_original);
+			j_original++;
+		}
+		i_original++;
+	}
+
+	imshow("Result image extention", result);
+	waitKey(0);
+
+	return result;
+}
+Mat Image_extension_average(Mat& mat, int x, int y)
+{
+	int average = 0;
+	for (int i = 0; i < mat.cols; i++)
+	{
+		for (int j = 0; j < mat.rows; j++)
+		{
+			average += mat.at<uint8_t>(j, i);
+		}
+	}
+
+	average = round(average / mat.cols / mat.rows);
+
+	Mat result(mat.rows + y - 1, mat.cols + x - 1, mat.type(), Scalar(average));
+
+	// Заполняем основную сцену нового изображения
+	int i_original = 0;
+	for (int i = round(y / 2); i < result.rows - round(y / 2); i++)
+	{
+		int j_original = 0;
+		for (int j = round(x / 2); j < result.cols - round(x / 2); j++)
+		{
+			result.at<uint8_t>(i, j) = mat.at<uint8_t>(i_original, j_original);
+			j_original++;
+		}
+		i_original++;
+	}
+
+	imshow("Result image extention", result);
+	waitKey(0);
+
+	return result;
+}
+Mat Image_extension_continue_image(Mat& mat, int x, int y)
+{
+	Mat result(mat.rows + y - 1, mat.cols + x - 1, mat.type(), Scalar(0));
+
+	// Заполняем основную сцену нового изображения
+	int i_original = 0;
+	for (int i = round(y / 2); i < result.rows - round(y / 2); i++)
+	{
+		int j_original = 0;
+		for (int j = round(x / 2); j < result.cols - round(x / 2); j++)
+		{
+			result.at<uint8_t>(i, j) = mat.at<uint8_t>(i_original, j_original);
+			j_original++;
+		}
+		i_original++;
+	}
+
+	// Заполняем края
+	for (int i = round(y / 2) - 1; i >= 0; i--)
+	{
+		for (int j = round(x / 2); j < result.cols - round(x / 2); j++)
+		{
+			result.at<uint8_t>(i, j) = result.at<uint8_t>(i + 1, j);
+		}
+	}
+	for (int i = result.rows - round(y / 2); i < result.rows; i++)
+	{
+		for (int j = round(x / 2); j < result.cols - round(x / 2); j++)
+		{
+			result.at<uint8_t>(i, j) = result.at<uint8_t>(i - 1, j);
+		}
+	}
+	for (int i = round(y / 2); i < result.rows - round(y / 2); i++)
+	{
+		for (int j = round(x / 2) - 1; j >= 0; j--)
+		{
+			result.at<uint8_t>(i, j) = result.at<uint8_t>(i, j + 1);
+		}
+	}
+	for (int i = round(y / 2); i < result.rows - round(y / 2); i++)
+	{
+		for (int j = result.cols - round(x / 2); j < result.cols; j++)
+		{
+			result.at<uint8_t>(i, j) = result.at<uint8_t>(i, j - 1);
+		}
+	}
+
+	// Заполняем углы
+	int value = result.at<uint8_t>(round(y / 2), round(x / 2));
+	//cout << value << endl;
+	for (int i = 0; i < round(y / 2); i++)
+	{
+		for (int j = 0; j < round(x / 2); j++)
+		{
+			result.at<uint8_t>(i, j) = value;
+		}
+	}
+
+	value = result.at<uint8_t>(round(y / 2), result.cols - round(x / 2) - 1);
+	//cout << value << endl;
+	for (int i = 0; i < round(y / 2); i++)
+	{
+		for (int j = result.cols - round(x / 2); j < result.cols; j++)
+		{
+			result.at<uint8_t>(i, j) = value;
+		}
+	}
+
+	value = result.at<uint8_t>(result.rows - round(y / 2) - 1, round(x / 2));
+	for (int i = result.rows - round(y / 2); i < result.rows; i++)
+	{
+		for (int j = 0; j < round(x / 2); j++)
+		{
+			result.at<uint8_t>(i, j) = value;
+		}
+	}
+
+	value = result.at<uint8_t>(result.rows - round(y / 2) - 1, result.cols - round(x / 2) - 1);
+	//cout << value << endl;
+	for (int i = result.rows - round(y / 2); i < result.rows; i++)
+	{
+		for (int j = result.cols - round(x / 2); j < result.cols; j++)
+		{
+			result.at<uint8_t>(i, j) = value;
+		}
+	}
+
+	imshow("Result image extention", result);
+	waitKey(0);
+
+	return result;
+}
+Mat Image_extension_reflect_image(Mat& mat, int x, int y)
+{
+	Mat result(mat.rows + y - 1, mat.cols + x - 1, mat.type(), Scalar(0));
+
+	if ((mat.rows * 2 < result.rows) || (mat.cols * 2 < result.cols))
+	{
+		cout << "Very big values for height or width" << endl;
+		exit(-2);
+	}
+
+	// Заполняем основную сцену нового изображения
+	int i_original = 0;
+	for (int i = round(y / 2); i < result.rows - round(y / 2); i++)
+	{
+		int j_original = 0;
+		for (int j = round(x / 2); j < result.cols - round(x / 2); j++)
+		{
+			result.at<uint8_t>(i, j) = mat.at<uint8_t>(i_original, j_original);
+			j_original++;
+		}
+		i_original++;
+	}
+
+	// Заполняем края
+	int i_value = round(y / 2);
+	for (int i = round(y / 2) - 1; i >= 0; i--)
+	{
+		for (int j = 0; j < result.cols; j++)
+		{
+			result.at<uint8_t>(i, j) = result.at<uint8_t>(i_value, j);
+		}
+		i_value++;
+	}
+	i_value = result.rows - round(y / 2) - 1;
+	for (int i = result.rows - round(y / 2); i < result.rows; i++)
+	{
+		for (int j = 0; j < result.cols; j++)
+		{
+			result.at<uint8_t>(i, j) = result.at<uint8_t>(i_value, j);
+		}
+		i_value--;
+	}
+	for (int i = 0; i < result.rows; i++)
+	{
+		int j_value = round(x / 2);
+		for (int j = round(x / 2) - 1; j >= 0; j--)
+		{
+			result.at<uint8_t>(i, j) = result.at<uint8_t>(i, j_value);
+			j_value++;
+		}
+	}
+	for (int i = 0; i < result.rows; i++)
+	{
+		int j_value = result.cols - round(x / 2) - 1;
+		for (int j = result.cols - round(x / 2); j < result.cols; j++)
+		{
+			result.at<uint8_t>(i, j) = result.at<uint8_t>(i, j_value);
+			j_value--;
+		}
+	}
+
+	imshow("Result image extention", result);
+	waitKey(0);
+
+	return result;
+}
+Mat Image_extension_back_reflect_image(Mat& mat, int x, int y)
+{
+	Mat result(mat.rows + y - 1, mat.cols + x - 1, mat.type(), Scalar(0));
+
+	if ((mat.rows * 2 < result.rows) || (mat.cols * 2 < result.cols))
+	{
+		cout << "Very big values for height or width" << endl;
+		exit(-2);
+	}
+
+	// Заполняем основную сцену нового изображения
+	int i_original = 0;
+	for (int i = round(y / 2); i < result.rows - round(y / 2); i++)
+	{
+		int j_original = 0;
+		for (int j = round(x / 2); j < result.cols - round(x / 2); j++)
+		{
+			result.at<uint8_t>(i, j) = mat.at<uint8_t>(i_original, j_original);
+			j_original++;
+		}
+		i_original++;
+	}
+
+	// Заполняем края
+	int i_value = result.rows - round(y / 2) - 1;
+	for (int i = round(y / 2) - 1; i >= 0; i--)
+	{
+		for (int j = 0; j < result.cols; j++)
+		{
+			result.at<uint8_t>(i, j) = result.at<uint8_t>(i_value, j);
+		}
+		i_value--;
+	}
+	i_value = round(y / 2);
+	for (int i = result.rows - round(y / 2); i < result.rows; i++)
+	{
+		for (int j = 0; j < result.cols; j++)
+		{
+			result.at<uint8_t>(i, j) = result.at<uint8_t>(i_value, j);
+		}
+		i_value++;
+	}
+	for (int i = 0; i < result.rows; i++)
+	{
+		int j_value = result.cols - round(x / 2) - 1;
+		for (int j = round(x / 2) - 1; j >= 0; j--)
+		{
+			result.at<uint8_t>(i, j) = result.at<uint8_t>(i, j_value);
+			j_value--;
+		}
+	}
+	for (int i = 0; i < result.rows; i++)
+	{
+		int j_value = round(x / 2);
+		for (int j = result.cols - round(x / 2); j < result.cols; j++)
+		{
+			result.at<uint8_t>(i, j) = result.at<uint8_t>(i, j_value);
+			j_value++;
+		}
+	}
+
+	imshow("Result image extention", result);
+	waitKey(0);
+
+	return result;
+}
+
+// square_x - длина прямоугольного окна
+// square_y - ширина прямоугольного окна
+// k - заранее подобранный коэффициент, определяющий значимость стандартного отклонения
+void Nibleck_binarization(Mat& mat, int square_x, int square_y, double k)
+{
+	if ((square_x % 2 == 0) || (square_y % 2 == 0))
+	{
+		cout << "Bad values of window" << endl;
+		exit(-1);
+	}
+	// Сначала необходимо "расширить" картинку одним из известных методов
+	//Image_extention_constant(mat, square_x, square_y, 128);
+	//Image_extension_average(mat, square_x, square_y);
+	//Mat extension_image = Image_extension_continue_image(mat, square_x, square_y);
+	//Mat extension_image = Image_extension_reflect_image(mat, square_x, square_y);
+	Mat extension_image = Image_extension_back_reflect_image(mat, square_x, square_y);
+
+	// Создаём матрицу со значениями порога для каждого пикселя
+	double** step_values = new double* [mat.cols];
+	//vector<vector<double>> step_values(mat.cols, vector<double>(mat.rows));
+
+
+	for (int i = 0; i < mat.cols; i++)
+	{
+		step_values[i] = new double[mat.rows];
+	}
+
+	int count_of_anomalous_values = 0;
+	int i_original = 0;
+	for (int i = round(square_y / 2); i < extension_image.rows - round(square_y / 2); i++)
+	{
+		int j_original = 0;
+		cout << "step = " << i << endl;
+		for (int j = round(square_x / 2); j < extension_image.cols - round(square_x / 2); j++)
+		{
+			int m = 0; // Среднее значение яркости пикселей в пределах данного окна
+
+			for (int ii = i - (square_y - 1) / 2; ii <= i + (square_y - 1) / 2; ii++)
+			{
+				for (int jj = j - (square_x - 1) / 2; jj <= j + (square_x - 1) / 2; jj++)
+				{
+					m += extension_image.at<uint8_t>(ii, jj);
+				}
+			}
+
+			m = round(m / (square_x * square_y));
+
+			double S = 0; // Стандартное отклонение
+
+			for (int ii = i - (square_y - 1) / 2; ii <= i + (square_y - 1) / 2; ii++)
+			{
+				for (int jj = j - (square_x - 1) / 2; jj <= j + (square_x - 1) / 2; jj++)
+				{
+					S += ((double)extension_image.at<uint8_t>(ii, jj) - m) * ((double)extension_image.at<uint8_t>(ii, jj) - m);
+				}
+			}
+
+			S = sqrt(S / ((double)square_x * (double)square_y));
+			//cout << "S = " << S << endl;
+
+			step_values[i_original][j_original] = m + k * S;
+			if ((step_values[i_original][j_original] < 0) || (step_values[i_original][j_original] > 255))
+			{
+				count_of_anomalous_values++;
+			}
+
+			j_original++;
+		}
+		i_original++;
+	}
+
+	cout << "All completed" << endl;
+	cout << "Count of anomalous values - " << count_of_anomalous_values << endl;
+
+	//for (int i = 0; mat.cols; i++)
+	//{
+	//	for (int j = 0; mat.rows; j++)
+	//	{
+	//		cout << step_values[i][j] << " ";
+	//	}
+	//	cout << endl;
+	//}
+
+	//// Процесс бинаризации
+	//for (int i = 0; i < mat.cols; i++)
+	//{
+	//	for (int j = 0; j < mat.rows; j++)
+	//	{
+	//		if (mat.at<uint8_t>(j, i) < step_values[i][j])
+	//		{
+	//			mat.at<uint8_t>(j, i) = 0;
+	//		}
+	//		else
+	//		{
+	//			mat.at<uint8_t>(j, i) = 255;
+	//		}
+	//	}
+	//}
+
+	//imshow("Binary cutie cat", mat);
+	//waitKey(0);
+}
 void Local_binarization(Mat& bw_mat)
 {
 	// Реализовать метод Ниблека локальной бинаризации
+	Nibleck_binarization(bw_mat, 5, 5, 0.2);
 }
 
 int main()
@@ -214,14 +597,20 @@ int main()
 	imshow("Black-white cat", binary_mat);
 	waitKey(0);
 
-	Mat result = Global_binarization(binary_mat);
+	cout << "Mini-result " << round(5 / 2) << endl;
 
-	imshow("Cutie cat", mat);
-	waitKey(0);
+	Local_binarization(binary_mat);
 
-	imshow("Black-white cutie cat", bw_mat);
-	waitKey(0);
+	//Mat result = Global_binarization(binary_mat);
 
-	imshow("Binarization cutie cat", result);
-	waitKey(0);
+	//imshow("Cutie cat", mat);
+	//waitKey(0);
+
+	//imshow("Black-white cutie cat", bw_mat);
+	//waitKey(0);
+
+	//imshow("Binarization cutie cat", result);
+	//waitKey(0);
+
+	return 0;
 }
